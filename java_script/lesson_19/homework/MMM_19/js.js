ea8edb757eaff0193251a8f7b992265c251a363f
Muhriddin <55646558+MuhriddinMMM@users.noreply.github.com>
2021-04-14 12:46:44 +0500

    ваыаыа

--- java_script/lesson_19/homework/MMM_19/js.js
+++ java_script/lesson_19/homework/MMM_19/js.js
@@ -27,6 +27,8 @@ btn.onclick = function (e) {
 
     let section = document.createElement("div");
     section.innerHTML = `
+    <div class="page">
+    <div data-num= ${+(i++ - 1)} class="num">
 	  <div class="result">
       <div class="img">
         <img src="${img}" alt="${img}" />
@@ -39,6 +41,8 @@ btn.onclick = function (e) {
           <button class="result__btn">Details</button>
         </div>
       </div>
+    </div>
+    </div>
     </div>
 	  `;
     second__block.append(section);
@@ -106,4 +110,59 @@ btn.onclick = function (e) {
   xhr.send();
 };
 
+var count = 10; //всего записей
+var cnt = 3; //сколько отображаем сначала
+var cnt_page = Math.ceil(count / cnt); //кол-во страниц
 
+//выводим список страниц
+var paginator = document.querySelector(".paginator");
+var page = "";
+for (var i = 0; i < cnt_page; i++) {
+  page +=
+    "<span data-page=" +
+    i * cnt +
+    '  id="page' +
+    (i + 1) +
+    '">' +
+    (i + 1) +
+    "</span>";
+}
+paginator.innerHTML = page;
+
+//выводим первые записи {cnt}
+var div_num = document.querySelectorAll(".num");
+for (var i = 0; i < div_num.length; i++) {
+  if (i < cnt) {
+    div_num[i].style.display = "block";
+  }
+}
+
+var main_page = document.getElementById("page1");
+main_page.classList.add("paginator_active");
+
+//листаем
+function pagination(event) {
+  var e = event || window.event;
+  var target = e.target;
+  var id = target.id;
+
+  if (target.tagName.toLowerCase() != "span") return;
+
+  var num_ = id.substr(4);
+  var data_page = +target.dataset.page;
+  main_page.classList.remove("paginator_active");
+  main_page = document.getElementById(id);
+  main_page.classList.add("paginator_active");
+
+  var j = 0;
+  for (var i = 0; i < div_num.length; i++) {
+    var data_num = div_num[i].dataset.num;
+    if (data_num <= data_page || data_num >= data_page)
+      div_num[i].style.display = "none";
+  }
+  for (var i = data_page; i < div_num.length; i++) {
+    if (j >= cnt) break;
+    div_num[i].style.display = "block";
+    j++;
+  }
+}
